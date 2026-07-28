13144522dd97e4cfac33e58fb1eb160d4b5a7156
Priyanshi1217 <yadavpriyanshi1217@gmail.com>
2026-07-28 10:45:46 +0530

    Day 1

---
+++ Day2.js
@@ -0,0 +1,32 @@
+// Synchromous:blocking js and asynchromous:nonblocking js
+// we'll use asynchromous js
+console.log("task 3");
+function hello(){
+    console.log("task 2");
+}
+hello();
+console.log("task 1");
+setTimeout(function(){
+    // Isse function 2-3 sec late load ho rha h
+console.log("task4");
+},2000)
+
+// callback:passed as an argument to another function and call
+function hello(n1,n2){
+    console.log("task1");
+    return n1+n2;
+}
+let a=10;
+let b =20;
+console.log(hello(a,b));
+hello(a,b,hi);
+hello(a,b,demo);
+function hi(){
+    console.log("sayhi");
+}
+hi();
+function demo(){
+    console.log("demo");
+}
+demo();
+
---
+++ es6.js
@@ -0,0 +1,16 @@
+// javascript
+// SyntaxErrorfunction
+// function fname(){
+//     block of CommandEvent}
+//     function calling
+// passing parameter in function
+    function sayHello(a,b){
+    console.log("cse 24 fsd");
+    return a+b;
+}
+console.log(sayHello(3,2));
+// arrow function
+// ()=>{}
+    console.log(sayHello=>()=>{
+        console.log("arrowfunction")
+    })
