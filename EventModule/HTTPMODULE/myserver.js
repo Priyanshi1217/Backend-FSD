180de41b331785e40e0fa18a1d883f27c2970e5e
Priyanshi1217 <yadavpriyanshi1217@gmail.com>
2026-08-21 12:44:21 +0530

    File

--- EventModule/HTTPMODULE/myserver.js
+++ EventModule/HTTPMODULE/myserver.js
@@ -1,10 +1,15 @@
-// create my own server using http module
 import http from "http";
-const server=http.createServer((req,res)=>{
-res.write("Welcome to my server");
-res.end();
-})
-server.listen(8000,()=>{
-console.log("Server is running on port 8000");
 
-})
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+
+    res.write("<h1>Hello CSE 24</h1>");
+    res.write("Welcome to my server");
+
+    res.end();
+});
+
+server.listen(8000, () => {
+    console.log("Server is running on port 8000");
+    console.log("Server is running on http://localhost:8000/");
+});
