a9cb5a2ab4e6411ef0ac2a53d4c7e268f37e64cd
Priyanshi1217 <yadavpriyanshi1217@gmail.com>
2026-09-08 10:16:44 +0530

    File change

---
+++ RESTAPI/File.js
@@ -0,0 +1,2 @@
+// Create a product RESTAPI and test all method in THUNDER CLIENT
+// Work it on approx 100 products and tell all the methods in THUNDER
--- RESTAPI/index.js
+++ RESTAPI/index.js
@@ -19,9 +19,28 @@ app.post('/users', (req, res) => {
     id: users.length + 1,
     name: req.body.name,
     email: req.body.email
-  };
-  users.push(user);
-  res.json(user);
+
+
+//   };
+//   users.push(user);
+//   res.json(user);
+// });
+
+       };
+          users.push(user);
+          res.json(user);
+
+})
+app.put( "/users/:id", (req,res) => {
+          let user = users.find(u=>u.id==req.params.id);
+          user.name = req.body.name;
+          user.email = req.body.email;
+          red.json(user);
+          //delete: request to delete a user
+          app.delete('/user/:id', (req,res) => {
+                    users = users.filter(u => u.id != req.params.id);
+                    res.send("user deleted successfully");
+          })
 });
 app.listen(8000, () => {
   console.log('Server is running on port 8000');
